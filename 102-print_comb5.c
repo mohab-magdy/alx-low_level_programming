ef99cb63ba63c619e28b242f00ffd16455af2fc3
MMAgdy <mmagdy.rabie@gmail.com>
2023-06-19 00:52:53 -0700

    Version-01

---
+++ 102-print_comb5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int i, j;
+
+  for (i = 0; i <= 99; i++) {
+    for (j = 0; j <= 99; j++) {
+      putchar('0' + (i / 10));
+      putchar('0' + (i % 10));
+      putchar(' ');
+      putchar('0' + (j / 10));
+      putchar('0' + (j % 10));
+      if (i != 99 || j != 99) {
+        putchar(',');
+        putchar(' ');
+      }
+    }
+  }
+
+  return 0;
+}
